170b48b8bfcabcfc05ba9ab8603a55a2f04130be
Dagimawii <dagiteach@gmail.com>
2024-12-19 19:29:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ setTimeout(() => {
           document.querySelector('.main').style.display = 'none';
         },2000);
          setTimeout(()=>{
-          window.location.href="BS.html";},3000);
+          window.location.href="Home_BQ.html";},3000);
 
     }
     else if(username =="" || password ==""){
